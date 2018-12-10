d58b651d06a60da716f11615f850e1e1871d7357
Martynas <mertynel65@gmail.com>
2018-12-10 22:07:57 +0200

    types

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("It works!");
+var bankAccount = {
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    }
+};
+var myself = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["  ", " "]
+};
+myself.bankAccount.deposit(3000);
+console.log();
--- app.ts
+++ app.ts
@@ -1,1 +1,18 @@
-console.log("It works!");
+type baccount = {money: number, deposit:(value: number) => void}
+
+let bankAccount:baccount = {
+    money: 2000,
+    deposit(value: number) {
+        this.money += value;
+    }    
+}
+
+let myself: {name: string, bankAccount:baccount, hobbies: Array<string>} = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["  ", " "]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log()
